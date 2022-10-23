3978620a785b5f164055e8ac09df147e7bdfcb1c
halim <113097676+Mhalim14@users.noreply.github.com>
2022-10-23 17:46:48 +0700

    Create us-muhammadHalimkasuma-22103001008.js

---
+++ uts-susulan/uts-susulan/us-muhammadHalimkasuma-22103001008.js
@@ -0,0 +1,16 @@
+var mahasiswa = [true, false];
+var ketertarikan = 1;
+
+mahasiswa.findIndex((value)=>{
+    if(value === false){
+        keterkarikan++
+    }
+})
+
+console.log(ketertarikan);
+if(ketertarikan >= 1){
+  console.log("teknik komputer")
+}
+else{
+  console.log("teknik industri")
+  }
